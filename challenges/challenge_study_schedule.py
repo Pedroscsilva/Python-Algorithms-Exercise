91164ee03cdf9c663e28ab7386af860cd658fbe4
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-05-25 14:58:34 -0300

    chore: completing the study bruteforce challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period, target_time=None):
+    if target_time is None:
+        return None
+
+    users_amount = 0
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            users_amount += 1
+
+    return users_amount
