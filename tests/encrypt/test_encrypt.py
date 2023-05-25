91f6acdf661dbd1adcc7d28f51ed942ee23137eb
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-05-25 15:20:38 -0300

    feat: adding a unit test for the message encryption algorithm

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("stringtest", 2) == "tsetgnir_ts"
+    assert encrypt_message("stringtest", 3) == "rts_tsetgni"
+    assert encrypt_message("stringtest", 0) == "tsetgnirts"
+    with pytest.raises(TypeError):
+        encrypt_message(123, 1)
+        encrypt_message("stringtest", "stringtest")
