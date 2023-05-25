9f95d270f46a26f6ed447776c9296222f7454021
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-05-25 16:56:26 -0300

    chore: completing the iterative palindromes challenge

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    for i, j in zip(range(0, len(word) - 1), range(len(word) - 1, 0, -1)):
+        if word[i] != word[j]:
+            return False
+    return True
